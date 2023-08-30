e9e67b906941d2750f960d58f5a04ac54a606fc7
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-08-30 16:12:51 -0400

    first commit

---
+++ src/assets/CollaboratorsDatabase.js
@@ -0,0 +1,26 @@
+export const collaboratorsDatabase = [
+    {
+        id: "1",
+        name: "Juan Soto",
+        email: "juans@colaborador.com",
+        age: "23",
+        position: "FrontEnd Developer",
+        phone: "99887766",
+    },
+    {
+        id: "2",
+        name: "Lucas Pailamilla",
+        email: "lucasp@colaborador.com",
+        age: "31",
+        position: "Backend Developer",
+        phone: "88779955",
+    },
+    {
+        id: "3",
+        name: "Diego Riquelme",
+        email: "diegor@colaborador.com",
+        age: "28",
+        position: "DevOps Engineer",
+        phone: "99226644",
+    },
+];
